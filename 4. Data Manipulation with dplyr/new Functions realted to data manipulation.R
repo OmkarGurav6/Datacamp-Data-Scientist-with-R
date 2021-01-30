7ac4bbac4fab5c313d60d984918655e7e187809d
Omkar Gurav <64888669+OmkarGurav6@users.noreply.github.com>
2021-01-30 15:55:05 +0530

    Add files via upload

---
+++ 4. Data Manipulation with dplyr/new Functions realted to data manipulation.R
@@ -0,0 +1,14 @@
+
+#2
+count()
+ungroup()
+top_n() # better alternative would be slice_max() ,gives highest values
+
+#3
+?select_helpers #used to select columns with diff combinations using select() function.
+rename()
+transmute()
+
+#4
+lag() # gives prev values in vector 
+
